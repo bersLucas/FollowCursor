b24dc6b316e13797b038cbab853bd3acec39ede2
Lucas Bersier <contact@lucasbersier.com>
2019-02-21 21:11:55 -0500

    Keep current css Transforms when applying.

--- src/followCursor.js
+++ src/followCursor.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * Initialization and rotate scripts
  *
  * elemList [HTML Collection]
@@ -12,7 +12,7 @@ const followCursor = function(elemList, scale=10) {
   //Init
   const boxSizes = [];
   let $elems = [];
-  
+
   if (Array.from !== undefined){
     $elems = Array.from(elemList);
   }else{
@@ -21,17 +21,17 @@ const followCursor = function(elemList, scale=10) {
       $elems[i] = elemList[i];
     }
   }
-  
+
   let counter = 0;
   let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  
+
   //Applied to every element in $elems
   $elems.reduce((total,img) => {
     counter += 1;
 
     //Bounding box
     let box = img.getBoundingClientRect();
-    
+
     //Object with data for element's position
     boxSizes[counter] = {
       left: box.left,
@@ -39,28 +39,21 @@ const followCursor = function(elemList, scale=10) {
       top: getElemDistance(img),
       height: box.height,
       obj: img,
-      classes: img.getAttribute("style"),
+      previousTransform: img.style.transform,
     }
-    
-    //Format style elements that existed before
-    if (boxSizes[counter].classes === null){
-      boxSizes[counter].classes = "";
-    }else{
-      boxSizes[counter].classes += ";"
-    }
-    
+
     //Apply a counter to find key in boxSizes array
     img.setAttribute("data-mouserotate",counter);
 
     //Applied on mouse over
-    img.onmousemove=function(e){ 
-      
+    img.onmousemove=function(e){
+
       //get key
       let count = this.getAttribute("data-mouserotate");
 
       //Cursor's X position in relation to the element (0-1)
       let xPos = (e.clientX - boxSizes[count].left) / boxSizes[count].width;
-      
+
       //Cursor's Y position in relation to the element (0-1)
       let yPos = ((scrollPos + e.clientY) - boxSizes[count].top) / boxSizes[count].height;
 
@@ -70,15 +63,19 @@ const followCursor = function(elemList, scale=10) {
 
       //Limit rotation to scale
       if (rotateY > scale){rotateY = scale}
-      if (rotateY < (scale * -1)){rotateY = (scale * -1)}      
+      if (rotateY < (scale * -1)){rotateY = (scale * -1)}
       if (rotateX > scale){rotateX = scale}
       if (rotateX < (scale * -1)){rotateX = (scale * -1)}
 
       //Apply rotation
-      img.setAttribute("style",`${boxSizes[count].classes}; ${transformCSS(boxSizes[count].height,rotateX.toFixed(3),rotateY.toFixed(3))}`);
+      console.log(boxSizes[img.getAttribute('data-mouserotate')].previousTransform +
+        transformCSS(boxSizes[count].height,rotateX.toFixed(3),rotateY.toFixed(3)))
+
+      img.style.transform = boxSizes[img.getAttribute('data-mouserotate')].previousTransform +
+        transformCSS(boxSizes[count].height,rotateX.toFixed(3),rotateY.toFixed(3))
     }
   },0);
-  
+
   //Reset element's information
   //(On resize or scroll)
   const resetSizes = function() {
@@ -89,10 +86,10 @@ const followCursor = function(elemList, scale=10) {
       boxSizes[i].height = boxRect.height;
       boxSizes[i].top = getElemDistance(box.obj);
     },0);
-    
+
     scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
   }
-  
+
   window.onscroll = resetSizes;
   window.onresize = resetSizes;
 };
@@ -112,8 +109,5 @@ const getElemDistance = function (elem) {
 
 //Function to add prefixes to transform rules
 const transformCSS = function(perspective,rotateX,rotateY){
-  let css = `transform: perspective(${perspective}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg);`;
-  css += `-webkit-transform: perspective(${perspective}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg);`;
-  css += `-moz-transform: perspective(${perspective}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg);`;
-  return css;
+  return `perspective(${perspective}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
 };
